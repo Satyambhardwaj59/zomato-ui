6f7a1e0a66a11f33cc8ce43c33882ea10778c191
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-01-27 21:20:29 +0530

    Stop this project due to insufficient data comming through it

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,24 +1,11 @@
-// import { resList } from "../utils/mookData";
-import { MOOK_URL } from "../utils/constant";
 import RestroCart from "./RestroCart";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { MOOK_URL } from "../utils/constant";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-
     const [listOfRestro, setListOfRestro] = useState([]);
-    const [filteredRestaurant, setFilteredRestaurant] = useState([]);
-
-    const [searchText, setSearchText] = useState("");
-
-    const setData = () => {
-        const filteredData = listOfRestro.filter(
-            (res) => res.info.avgRating > 4
-        );
-        setListOfRestro(filteredData);
-        
-    }
-
+    
     useEffect(() => {
         fetchData();
     }, []);
@@ -26,40 +13,27 @@ const Body = () => {
     const fetchData = async () => {
         const data = await fetch(MOOK_URL);
         const jsonData = await data.json();
-        console.log(jsonData);
-
-        setListOfRestro(jsonData);
-        setFilteredRestaurant(jsonData);
         
+        setListOfRestro(jsonData.data.data);
     }
 
-    
-
     return listOfRestro.length === 0 ? <Shimmer/> : (
+        
         <div className="body">
             <div className="search">
-                <input type="text" placeholder="Find your Restro..."  className="search-box" value={searchText} onChange={(e) => {
-                    setSearchText(e.target.value);
-                }}/>
-                <button onClick={
-                    () => {
-                        console.log(searchText);
-
-                        const filteredRestro = filteredRestaurant.filter((res) => {res.brand_name.toLowerCase().includes(searchText.toLowerCase())})
-                        setFilteredRestaurant(filteredRestro);
-                    }
-                }>Search</button>
-                <button className="filter-btn" onClick={setData}>Top Rated Restro..</button>
+                <input type="text" placeholder="Find your Restro..." />
+                <button>Search</button>
+               
             </div>
             <div className="res-container">
                 {
-                    filteredRestaurant.map(restaurant => (
-                        <RestroCart key={restaurant.info.brand_id} resData = {restaurant} />
+                    listOfRestro.map((restaurant) => (             
+                        <RestroCart key={restaurant.brand_id} resData = {restaurant} />
                     ))
-                }
-            </div>
-        </div>
-    )
-}
+                }   
+             </div>   
+        </div>   
+    )   
+}   
 
 export default Body;
--- src/Components/RestroCart.js
+++ src/Components/RestroCart.js
@@ -1,23 +1,20 @@
 import { background } from "../utils/constant";
-import { MOOK_URL } from "../utils/constant";
+
 
 const RestroCart = (props) => {
 
     const {resData} = props;
 
-    const {store_front_brand_image_url, brand_name, description, costForTwo, avgRating, deliveryTime} = resData?.info;
+    const {store_front_brand_image_url, brand_name, description} = resData
 
     return (
         <div className="cart" style={background}>
             <div className="res-img">
-                <img src={store_front_brand_image_url} alt="cart img"  />
+                <img src={store_front_brand_image_url ? store_front_brand_image_url : "https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5297921.png" } alt="cart img"  />
             </div>
             <div className="cart-desc">                
                 <h3>{brand_name}</h3>
                 <h4>{description}</h4>
-                {/* <h4>{costForTwo + " Cost For Two"}</h4>
-                <h4>{avgRating + " star"}</h4>
-                <h4>{" Delivary time " + deliveryTime + " min"}</h4> */}
             </div>
         </div>
     )
